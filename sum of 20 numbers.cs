00c6218c035fff43c5be2623ba69e58b8bda552b
ArefehSoltani0 <soltaniarefe86@gmail.com>
2025-11-07 18:05:18 +0330

    Add files via upload

---
+++ sum of 20 numbers.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace HelloWold;
+
+public static class Program
+{
+    public static void Main()
+    {
+
+        int x, y;
+        for (int i = 0; i < 20; i++)
+        {
+            Console.WriteLine("enter number 1");
+            x = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter number2");
+            y = int.Parse(Console.ReadLine());
+            Console.WriteLine("{0}+{1}={2}", x, y, (x + y));
+        }
+        Console.ReadKey();
+    }
+}
